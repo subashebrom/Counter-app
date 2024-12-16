9624a478ead9c94ef9d46d7a7f38acc1ea589b1e
subashebrom <subashembrom99@gmail.com>
2024-12-16 14:18:30 +0600

    Add files via upload

---
+++ js/counter.js
@@ -0,0 +1,10 @@
+let sum =0;
+function plusclick(){
+    sum = sum + 1;
+    document.getElementById('total').innerText = sum;
+}
+let sub =0;
+function lessclick(){
+    sub = sub - 1;
+    document.getElementById('total').innerText = sub;
+}
